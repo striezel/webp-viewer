c3b924b43b177f990992d4dc35b49229a579d246
Dirk Stolle <striezel-dev@web.de>
2022-06-27 19:39:23 +0200

    adjust CMake scripts to enable more compiler warnings

--- webp-viewer/main.cpp
+++ webp-viewer/main.cpp
@@ -54,7 +54,8 @@ void showHelp()
             << "  FILE            - Sets the file name of image to show.\n";
 }
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+void key_callback(GLFWwindow* window, int key, [[maybe_unused]] int scancode,
+                  int action, [[maybe_unused]] int mods)
 {
    if ((key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) && action == GLFW_PRESS)
      glfwSetWindowShouldClose(window, GLFW_TRUE);
